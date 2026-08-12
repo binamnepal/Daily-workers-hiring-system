6b74d619caa357e8cd3eac0d570ce9f680388c66
Binam Nepal <binamnepal173@gmail.com>
2026-08-12 10:56:33 +0545

    first commit

---
+++ daily_workers_hiring/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for daily_workers_hiring project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'daily_workers_hiring.settings')
+
+application = get_asgi_application()
