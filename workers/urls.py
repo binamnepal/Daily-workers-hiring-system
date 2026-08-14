bd9ff89ba15fcc03f621cf9c7af849dbc3b67c39
Binam Nepal <binamnepal173@gmail.com>
2026-08-14 10:20:37 +0545

    last main change

--- workers/urls.py
+++ workers/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("me/", views.my_profile, name="my_profile"),
     path("me/availability/", views.add_availability, name="add_availability"),
     path("<uuid:id>/", views.worker_detail, name="detail"),
+    path("search/", views.search, name="search"),
 ]
